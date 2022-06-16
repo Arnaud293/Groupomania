38e125155c5ac1e4219bcf7294e8e94b57b155dc
Arnaud293 <arnaudlt293@gmail.com>
2022-06-16 13:44:51 +0200

    add salt to the password at users registering

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,6 +27,14 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Encrypt the password
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
