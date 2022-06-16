5bd1e996427de4ed35f719e914162f5978fdced0
Arnaud293 <arnaudlt293@gmail.com>
2022-06-16 12:39:04 +0200

    add the auth.controller with SignUp function

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const UserModel = require("../models/user.model");
+
+module.exports.signUp = async (req, res) => {
+  const { pseudo, email, password } = req.body;
+
+  try {
+    const user = await UserModel.create({ pseudo, email, password });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    res.status(200).send({ err });
+  }
+};
