aeeeafa187e77ff4aea0847fd2a5acd3244d97a6
Arnaud293 <arnaudlt293@gmail.com>
2022-06-17 13:42:03 +0200

    CRUD follow controller is working

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,3 +15,79 @@ module.exports.getOneUser = (req, res) => {
     else console.log("Id Unknown : " + err);
   }).select("-password");
 };
+
+module.exports.updateUser = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("Id unkown : " + req.params.id);
+
+  try {
+    UserModel.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          bio: req.body.bio,
+        },
+      },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        if (err) return res.status(500).send({ message: err });
+      }
+    );
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+module.exports.deleteUser = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("Id unkown : " + req.params.id);
+
+  try {
+    UserModel.deleteOne({ _id: req.params.id }).exec();
+    res.status(200).json({ message: "User deleted ! " });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+module.exports.follow = (req, res) => {
+  if (
+    !ObjectId.isValid(req.params.id) ||
+    !ObjectId.isValid(req.body.idToFollow)
+  )
+    return res.status(400).send("Id unkown : " + req.params.id);
+  try {
+    // to the follower list :
+    UserModel.findByIdAndUpdate(
+      req.params.id,
+      { $addToSet: { following: req.body.idToFollow } },
+      { new: true, upsert: true },
+      (err, docs) => {
+        if (!err) res.status(201).json(docs);
+        else return res.status(400).json(err);
+      }
+    );
+    // to the following list :
+
+    UserModel.findByIdAndUpdate(
+      req.body.idToFollow,
+      { $addToSet: { followers: req.params.id } },
+      { new: true, upsert: true },
+      (err, docs) => {
+        if (err) return res.status(400).json(err);
+      }
+    );
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+module.exports.unfollow = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("Id unkown : " + req.params.id);
+  try {
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
