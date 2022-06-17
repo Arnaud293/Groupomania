212a0a29066503dcf81ff1978505c13839dde24f
Arnaud293 <arnaudlt293@gmail.com>
2022-06-17 13:40:50 +0200

    CRUD patch for follow/unfollow

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,5 +9,9 @@ router.post("/register", authController.signUp);
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getOneUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
 
 module.exports = router;
