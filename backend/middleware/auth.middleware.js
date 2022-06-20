d8dbba35a56c0f2f78a264ac4cbdc94ed86e7b16
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 14:36:14 +0200

    AUTH: add requireAuth(token?) for first connection

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -19,3 +19,19 @@ module.exports.checkUser = (req, res, next) => {
     next();
   }
 };
+
+module.exports.requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(decodedToken.id);
+        next();
+      }
+    });
+  } else {
+    console.log("No token");
+  }
+};
