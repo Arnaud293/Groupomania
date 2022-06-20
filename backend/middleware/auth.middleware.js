e58843300e47855d26f6b2efb58a3bbd8a288b4b
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 14:10:24 +0200

    AUTH : checkUser - jwt.verify

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model");
+
+module.exports.checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        res.cookie("jwt", "", { maxAge: 1 });
+      } else {
+        let user = await UserModel.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
