1c1a6c541e9bab8b80d213f5ca5959fa1f306c11
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 12:21:15 +0200

    AUTH: add login/logout

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,8 @@ const userController = require("../controllers/user.controller");
 
 // auth
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
 // user db
 
