63d83d7d45c1c8c572699c668d3222af0beb3755
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 14:11:47 +0200

    MAJ: auth.middleware for req : get(*)

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const { json } = require("express");
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 
+const checkUser = require("./middleware/auth.middleware");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// jwt
+
+app.get("*", checkUser);
 
 // Routes
 
