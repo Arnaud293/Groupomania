7271959cd036a7971134b4a26a5411d91358ee04
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 12:23:57 +0200

    AUTH : decrypt token for login

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -36,5 +36,17 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect password");
+  }
+  throw Error("Incorrect password");
+};
+
 const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
