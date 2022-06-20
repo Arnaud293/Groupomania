a31a25142573e2813c39eb483805d5c684d8eb2c
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 16:59:43 +0200

    ERR: add signInErrors management

--- backend/utils/errors.utils.js
+++ backend/utils/errors.utils.js
@@ -20,3 +20,16 @@ module.exports.signUpErrors = (err) => {
 
   return errors;
 };
+
+module.exports.signInErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  if (err.message.includes("email")) {
+    errors.email = "Email inconnu";
+  }
+  if (err.message.includes("password")) {
+    errors.password = "Mot de passe incorrect";
+  }
+
+  return errors;
+};
