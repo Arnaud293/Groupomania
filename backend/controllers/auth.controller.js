22bfcfed81eff25eb691ca828197e1199954e127
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 12:22:45 +0200

    AUTH : SignIn + token(cookie)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,13 @@
 const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
+};
 
 module.exports.signUp = async (req, res) => {
   const { pseudo, email, password } = req.body;
@@ -10,3 +19,18 @@ module.exports.signUp = async (req, res) => {
     res.status(200).send({ err });
   }
 };
+
+module.exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(200).json(err);
+  }
+};
+
+module.exports.logout = (req, res) => {};
