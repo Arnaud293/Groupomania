f7d7830651ff6e7915521648a681f1d25ecd9ed3
Arnaud293 <arnaudlt293@gmail.com>
2022-06-20 17:34:34 +0200

    CRUD-POST : Add the post model before crud

---
+++ backend/models/post.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+
+const PostSchema = new mongoose.Schema(
+    {
+        posterId : {type : String, required : true},
+        message : {type : String, trim : true, maxlength : 500},
+        picture:  {type : String},
+        video : {type: String},
+        likers : {type : [String], required : true},
+        comments :{type: [
+            {
+                commenterId : String,
+                commenterPseudo : String,
+                text : String,
+                timestamp : number,
+            }
+        ],
+        required : true},
+    
+
+    }
+    {timestamp : true}
+    )
