efaba10c85f201982d6e8010ade48ad7539f5908
Arnaud293 <arnaudlt293@gmail.com>
2022-06-21 16:57:26 +0200

    CRUD-POST : preparing roads for commentPost/editCommentPost/deleteCommentPost

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -8,4 +8,10 @@ router.delete("/:id", postController.deletePost);
 router.patch("/like-post/:id", postController.likePost);
 router.patch("/unlike-post/:id", postController.unlikePost);
 
+// Comment
+
+router.patch("/comment-post/:id", postController.commentPost);
+router.patch("/edit-comment-post/:id", postController.editCommentPost);
+router.patch("/delete-comment-post/:id", postController.deleteCommentPost);
+
 module.exports = router;
