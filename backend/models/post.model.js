a55123364e1953e1b27b99a028bc0cd83432f74e
Arnaud293 <arnaudlt293@gmail.com>
2022-06-21 10:24:25 +0200

    FIX:  number was in lowercase

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-
 const PostSchema = new mongoose.Schema(
-    {
-        posterId : {type : String, required : true},
-        message : {type : String, trim : true, maxlength : 500},
-        picture:  {type : String},
-        video : {type: String},
-        likers : {type : [String], required : true},
-        comments :{type: [
-            {
-                commenterId : String,
-                commenterPseudo : String,
-                text : String,
-                timestamp : number,
-            }
-        ],
-        required : true},
-    
+  {
+    posterId: { type: String, required: true },
+    message: { type: String, trim: true, maxlength: 500 },
+    picture: { type: String },
+    video: { type: String },
+    likers: { type: [String], required: true },
+    comments: {
+      type: [
+        {
+          commenterId: String,
+          commenterPseudo: String,
+          text: String,
+          timestamp: Number,
+        },
+      ],
+      required: true,
+    },
+  },
 
-    }
-    {timestamp : true}
-    )
+  { timestamp: true }
+);
+
+const PostModel = mongoose.model("post", PostSchema);
+module.exports = PostModel;
