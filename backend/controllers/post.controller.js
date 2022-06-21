8cfe2bb6086240ec745a589db49e699e8de481d8
Arnaud293 <arnaudlt293@gmail.com>
2022-06-21 16:56:24 +0200

    CRUD-POST : add commentPost and editCommentPost

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -10,7 +10,7 @@ module.exports.readPost = (req, res) => {
     } else {
       console.log("Error to get data" + err);
     }
-  });
+  }).sort({ createdAt: -1 });
 };
 
 module.exports.createPost = async (req, res) => {
@@ -110,4 +110,62 @@ module.exports.unlikePost = async (req, res) => {
   } catch (err) {
     return res.status(400).send(err);
   }
+};
+
+module.exports.commentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("Id unkown : " + req.params.id);
+
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.editCommentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("Id unkown : " + req.params.id);
+
+  try {
+    return PostModel.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) =>
+        comment._id.equals(req.body.commentId)
+      );
+
+      if (!theComment) {
+        return res.status(404).send("comment not found");
+      } else {
+        theComment.text = req.body.text;
+      }
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        else return res.status(500).send(err);
+      });
+    });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.deleteCommentPost = (req, res) => {
+  if (!ObjectId.isValid(req.params.id))
+    return res.status(400).send("Id unkown : " + req.params.id);
 };
