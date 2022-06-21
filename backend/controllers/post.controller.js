bf3681166c268e84e1984e1bd7e17fed1826556b
Arnaud293 <arnaudlt293@gmail.com>
2022-06-21 10:21:37 +0200

    CRUD-POST : preparing empty function (prevent crash)

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,9 @@
+const { default: mongoose } = require("mongoose");
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectId = require("mongoose").Types.ObjectId;
+
+module.exports.readPost = (req, res) => {};
+module.exports.createPost = (req, res) => {};
+module.exports.updatePost = (req, res) => {};
+module.exports.deletePost = (req, res) => {};
