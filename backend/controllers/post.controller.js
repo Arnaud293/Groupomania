ff8eb92f44e86ec76e97296ca37e348ffbaaf41e
Arnaud293 <arnaudlt293@gmail.com>
2022-06-21 11:13:40 +0200

    CRUD-POST : readPost(get) & createPost(post)

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -3,7 +3,32 @@ const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 const ObjectId = require("mongoose").Types.ObjectId;
 
-module.exports.readPost = (req, res) => {};
-module.exports.createPost = (req, res) => {};
+module.exports.readPost = (req, res) => {
+  PostModel.find((err, docs) => {
+    if (!err) {
+      res.send(docs);
+    } else {
+      console.log("Error to get data" + err);
+    }
+  });
+};
+
+module.exports.createPost = async (req, res) => {
+  const newPost = new PostModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    video: req.body.video,
+    likers: [],
+    comments: [],
+  });
+  try {
+    const post = await newPost.save();
+    return res.status(200).json(post);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
 module.exports.updatePost = (req, res) => {};
+
 module.exports.deletePost = (req, res) => {};
