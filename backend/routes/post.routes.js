7e2484d54541744c8dc3af3deec501b185740d78
Arnaud293 <arnaudlt293@gmail.com>
2022-06-21 14:57:15 +0200

    CRUD-POST : preparing patch roads for like/unlike post

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -5,5 +5,7 @@ router.get("/", postController.readPost);
 router.post("/", postController.createPost);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
+router.patch("/like-post/:id", postController.likePost);
+router.patch("/unlike-post/:id", postController.unlikePost);
 
 module.exports = router;
