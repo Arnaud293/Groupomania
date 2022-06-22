833c23d613473a3e0a63dd0cbf710063e2c84d53
Arnaud293 <arnaudlt293@gmail.com>
2022-06-22 15:16:12 +0200

    UPLOAD: preparing roads for uploading profil picture

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
 // auth
 router.post("/register", authController.signUp);
@@ -15,5 +18,9 @@ router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
+
+// upload
+
+router.post("/upload", upload.single("file"), uploadController.uploadProfil);
 
 module.exports = router;
