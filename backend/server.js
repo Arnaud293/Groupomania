a67febf18421f3409c8fc9f451573074ad4d806f
Arnaud293 <arnaudlt293@gmail.com>
2022-06-22 16:51:45 +0200

    CORS: set corsOptions

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,20 @@ require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 
 const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
