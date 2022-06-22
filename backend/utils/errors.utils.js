33ad9c0389f901393f282d9356f3e70f4644180e
Arnaud293 <arnaudlt293@gmail.com>
2022-06-22 16:50:31 +0200

    ERR : uploadErrors fix

--- backend/utils/errors.utils.js
+++ backend/utils/errors.utils.js
@@ -34,4 +34,15 @@ module.exports.signInErrors = (err) => {
   return errors;
 };
 
-module.exports.uploadErrors = (err) => {};
+module.exports.uploadErrors = (err) => {
+  let errors = { format: "", maxSize: "" };
+
+  if (err.message.includes("Invalid file")) {
+    errors.format = "Format incompatible";
+  }
+  if (err.message.includes("max size")) {
+    errors.maxSize = "Le fichier doit faire moins de 500ko";
+  }
+
+  return errors;
+};
