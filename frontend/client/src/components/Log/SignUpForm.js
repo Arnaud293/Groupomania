82ec77541faa75a73be36f1780695676860707e9
Arnaud293 <arnaudlt293@gmail.com>
2022-07-01 11:53:32 +0200

    AUTH: html structure is ready

--- frontend/client/src/components/Log/SignUpForm.js
+++ frontend/client/src/components/Log/SignUpForm.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const SignUpForm = () => {
   return (
-    <div>SignUpForm</div>
+    <div class="connection-form-card">
+        <form class="connection-form">
+            <label for="text">Email</label>
+            <input class="input" type="text" name="text"/>
+            <br/>
+            <label for="text">Pseudo</label>
+            <input class='input' type="text" name="text"/>
+            <br/>
+            <label for="text">Mot de passe</label>
+            <input class='input' type="text" name="text"/>
+            <br/>
+            <label for="text">Confirmez votre mot de Passe</label>
+            <input class='input' type="text" name="text"/>
+            <br/>
+            <input type="submit" value="Connexion" class="main-btn"/>
+        </form>
+        
+    </div>
   )
 }
 
