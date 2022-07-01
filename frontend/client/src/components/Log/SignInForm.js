55e426b2ca42d06124f110a4b2ac50c3ce9cfac7
Arnaud293 <arnaudlt293@gmail.com>
2022-07-01 11:52:02 +0200

    AUTH : SignIn is working, path to /home

--- frontend/client/src/components/Log/SignInForm.js
+++ frontend/client/src/components/Log/SignInForm.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const SignInForm = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin= (e) =>{
+      e.preventDefault();
+      const emailError = document.querySelector('.email-error');
+      const passwordError = document.querySelector('.password-error');
+
+      axios({
+        method : 'post',
+        url: `${process.env.REACT_APP_API_URL}api/user/login`,
+        withCredentials : true,
+        data: {
+          email,
+          password,
+        }
+      })
+
+      .then((res) => {
+        console.log(res.data.errors);
+        if(res.data.errors){
+          emailError.innerHTML= res.data.errors.email;
+          passwordError.innerHTML= res.data.errors.password;
+        } else {
+          window.location = '/home'
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    };
+
   return (
-    <div>SignInForm</div>
+    <div class="connection-form-card">
+        <form action="" onSubmit={handleLogin} class="connection-form">
+            <label htmlFor="email">Email</label>
+            <input class="input" type="text" name="text" id='email' onChange={(e) => setEmail(e.target.value)} value={email}/>
+            <div className='email-error'></div>
+            <br/>
+            <label htmlFor="password">Mot de passe</label>
+            <input class='input' type="password" name="password" id='password' onChange={(e) => setPassword(e.target.value)} value={password}/>
+            <div className='password-error'></div>
+            <br/>
+            <input type="submit" value="Connexion" class="main-btn"/>
+        </form>
+    
+    </div>
   )
 }
 
-export default SignInForm
+export default SignInForm;
