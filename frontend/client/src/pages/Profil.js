c92013c2bb04e944e5e1c76689bcccc5b0965f5c
Arnaud293 <arnaudlt293@gmail.com>
2022-07-01 17:45:59 +0200

    BASE: preparing header and nav

--- frontend/client/src/pages/Profil.js
+++ frontend/client/src/pages/Profil.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UidContext } from '../components/AppContext';
+
+// SRC
+import HomeIcon from '../../src/src/icons/home.svg';
+import CompanyLogo from '../../src/src/logo/icon-left-font-small.png';
+import TrendingIcon from '../../src/src/icons/hot.svg';
+import ProfilIcon from '../../src/src/icons/user.svg';
+import LogoutIcon from '../../src/src/icons/circle-logout.svg';
 
 const Profil = () => {
+
+  const uid = useContext(UidContext);
+
   return (
-    <div>Profil</div>
+    <div className='profil'>
+      <header class="main-pages-header">
+        <div class="main-pages-header-container">
+          <img src={CompanyLogo} alt="company-logo"/>
+          <h1 class="main-pages-header-title">Groupomania</h1>
+        </div>
+        <img class="main-pages-header-icon" src={LogoutIcon} />
+        
+    </header>
+    <nav class="left-nav-container">
+        <div class="left-nav-icons">
+          <img class='selected-nav-element' src={HomeIcon} alt="home-icon"/>
+          <img src={TrendingIcon} alt="trending-icon"/>
+          <img src={ProfilIcon} alt='profil-icon'/>
+        </div>
+    </nav>
+    {uid ? (
+      <h1>PAGE DU PROFIL</h1>
+    ) : (
+      window.location('/')
+    )}
+
+    </div>
   )
 }
 
