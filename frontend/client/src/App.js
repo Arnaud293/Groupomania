710997cf1d8ffd718998a99a19503979f5323c0a
Arnaud293 <arnaudlt293@gmail.com>
2022-07-02 18:10:59 +0200

    REDUX: dispatch userData to the store

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,12 +1,38 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { useEffect, useState } from "react";
 import Routes from "./components/Routes/index";
+import {UidContext} from './components/AppContext';
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user.actions";
+
+
 
 function App() {
+
+  const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    const fetchToken = async () => {
+    await axios({
+      method : 'get',
+      url : `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials : true
+    })
+    .then((res) => setUid(res.data))
+    .catch((err) => console.log('no-token'))
+
+    if(uid) dispatch(getUser(uid))}
+    fetchToken();
+
+  }, [uid])
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Routes />
-    </div>
+    </UidContext.Provider>
   );
 }
 
