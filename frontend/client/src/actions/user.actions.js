39b9562284315a57fb436c20fd197550c34c402e
Arnaud293 <arnaudlt293@gmail.com>
2022-07-02 18:06:52 +0200

    REDUX: first action getUser data

---
+++ frontend/client/src/actions/user.actions.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const GET_USER = 'GET_USER';
+
+export const getUser = (uid) =>{
+    return (dispatch) => {
+        return axios
+        .get(`${process.env.REACT_APP_API_URL}api/user/${uid}`)
+        .then((res) => {
+            dispatch({type: GET_USER, payload: res.data})
+        })
+        .catch((err) => console.log(err));
+    }
+}
