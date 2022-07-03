116771366efb3766d1676be34a932cea07911a0e
Arnaud293 <arnaudlt293@gmail.com>
2022-07-03 13:08:43 +0200

    BUILD: add updateProfil and the login system if != uid

--- frontend/client/src/pages/Profil.js
+++ frontend/client/src/pages/Profil.js
@@ -2,31 +2,49 @@ import React, { useContext } from 'react';
 import { UidContext } from '../components/AppContext';
 
 // SRC
-import HomeIcon from '../../src/src/icons/home.svg';
-import TrendingIcon from '../../src/src/icons/hot.svg';
-import ProfilIcon from '../../src/src/icons/user.svg';
+// import HomeIcon from '../../src/src/icons/home.svg';
+// import TrendingIcon from '../../src/src/icons/hot.svg';
+// import ProfilIcon from '../../src/src/icons/user.svg';
 import Navbar from '../components/Navbar';
+import UpdateProfil from '../components/Profil/UpdateProfil';
+import Log from '../components/Log';
+
+// SRC
+import Background from '../../src/src/img/connection-background.jpg';
+import Logo from '../../src/src/logo/icon-left-font-monochrome-white.svg';
+import LogoSmall from '../../src/src/logo/icon-left-font-monochrome-white-petit.png';
 
 const Profil = () => {
 
   const uid = useContext(UidContext);
 
   return (
-    <div className='profil'>
-    <Navbar />
-    <nav class="left-nav-container">
-        <div class="left-nav-icons">
-          <img class='selected-nav-element' src={HomeIcon} alt="home-icon"/>
-          <img src={TrendingIcon} alt="trending-icon"/>
-          <img src={ProfilIcon} alt='profil-icon'/>
-        </div>
-    </nav>
-    {/* {uid ? (
-      <h1>PAGE DU PROFIL</h1>
-    ) : (
-      window.location('/')
-    )} */}
+    <div>
+      {uid ? (
+      <div className='profil'>
+     
+      <Navbar />
+      <UpdateProfil />
+      
+      </div>
+      ): (
+        <div className='connection'>
+        <img src={Background} alt="team-pic" class='background-img'/>
+      <header>
+      <div class="connection-page-logo">
+          <img src={Logo} alt="logo" />
+      </div>
+      <div class="connection-page-logo-small">
+          <img src={LogoSmall} alt="logo" />
+      </div>
+      </header>
+      
 
+      <Log signin={true} signup={false}/>
+
+    </div>
+      )}
+    
     </div>
   )
 }
