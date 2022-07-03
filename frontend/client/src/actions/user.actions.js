b35fa341bc1f60add57f948a439655978b748c90
Arnaud293 <arnaudlt293@gmail.com>
2022-07-03 16:19:40 +0200

    UPLOAD: add case UPLOAD_PITURE

--- frontend/client/src/actions/user.actions.js
+++ frontend/client/src/actions/user.actions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_USER = 'GET_USER';
+export const UPLOAD_PICTURE = 'UPLOAD_PICTURE';
 
 export const getUser = (uid) =>{
     return (dispatch) => {
@@ -8,6 +9,21 @@ export const getUser = (uid) =>{
         .get(`${process.env.REACT_APP_API_URL}api/user/${uid}`)
         .then((res) => {
             dispatch({type: GET_USER, payload: res.data});
+        })
+        .catch((err) => console.log(err));
+    }
+}
+
+export const uploadPicture = (data, id) =>{
+    return (dispatch) => {
+        return axios
+        .post(`${process.env.REACT_APP_API_URL}api/user/upload`, data)
+        .then((res) => {
+            return axios
+            .get(`${process.env.REACT_APP_API_URL}api/user/${id}`)
+            .then((res) => {
+                dispatch({type : UPLOAD_PICTURE, payload : res.data.picture})
+            })
         })
         .catch((err) => console.log(err));
     }
--- frontend/client/src/reducers/user.reducer.js
+++ frontend/client/src/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import {GET_USER} from '../actions/user.actions';
+import {GET_USER, UPLOAD_PICTURE} from '../actions/user.actions';
 
 const initialState = {};
 
@@ -6,6 +6,11 @@ export default function userReducer(state = initialState, action){
     switch (action.type) {
         case GET_USER:
             return action.payload;
+        case UPLOAD_PICTURE:
+            return {
+                ...state,
+                picture : action.payload
+            }
 
     default: return state;
     }
