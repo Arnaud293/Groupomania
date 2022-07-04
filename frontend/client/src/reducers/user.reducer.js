d98a64ed7d26fe39f0654d51e09deb56bd7ccca4
Arnaud293 <arnaudlt293@gmail.com>
2022-07-04 13:06:20 +0200

    REDUX: case UPDATE_FORM

--- frontend/client/src/reducers/user.reducer.js
+++ frontend/client/src/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import {GET_USER, UPLOAD_PICTURE} from '../actions/user.actions';
+import {GET_USER, UPDATE_BIO, UPLOAD_PICTURE} from '../actions/user.actions';
 
 const initialState = {};
 
@@ -10,6 +10,11 @@ export default function userReducer(state = initialState, action){
             return {
                 ...state,
                 picture : action.payload
+            }
+        case UPDATE_BIO :
+            return {
+                ...state,
+                bio: action.payload
             }
 
     default: return state;
