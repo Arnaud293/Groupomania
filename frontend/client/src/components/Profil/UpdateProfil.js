0acc34ecc66e3bf68f70b41415d45034f1e1ae75
Arnaud293 <arnaudlt293@gmail.com>
2022-07-04 13:05:26 +0200

    PUT: UpdateBio from <p> to <textarea> to <p> using REDUX

--- frontend/client/src/components/Profil/UpdateProfil.js
+++ frontend/client/src/components/Profil/UpdateProfil.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateBio } from '../../actions/user.actions';
 import LeftNav from '../LeftNav';
 import UploadImg from './UploadImg';
 
 const UpdateProfil = () => {
 
+ const [bio, setBio] = useState('');
+ const [updateForm, setUpdateForm] = useState(false);
 
  const userData = useSelector((state) => state.userReducer);  
+ const dispatch = useDispatch();
+
+ const handleUpdate = () => {
+    dispatch(updateBio(userData._id, bio));
+    setUpdateForm(false);
+ }
 
   return (
     <div  className='profil-container'>
@@ -28,12 +37,24 @@ const UpdateProfil = () => {
         <div className="main-profil-content-card">
             <h2>Biographie</h2>
             <div className="bio-container">
-                <textarea class="modify-bio"></textarea>
-                <div className="bio-btn-container">
-                    <input type="button" value="Modifier" className="bio-btn"/>
+                {updateForm === false && (
+                   <>
+                   <p class="modify-bio" onClick={() => setUpdateForm(!updateForm)} >{userData.bio}</p>
+                   <div className="bio-btn-container">
+                    <input type="button" value="Modifier" className="bio-btn" onClick={() => setUpdateForm(!updateForm)}/>
                 </div>
+                   </> 
+                )}
+                {updateForm && (
+                    <>
+                    <textarea type='text' class="modify-bio" defaultValue={userData.bio} onChange={(e) => setBio(e.target.value)} ></textarea>
+                    <div className="bio-btn-container">
+                        <input type="button" value="Valider" className="bio-btn" onClick={handleUpdate}/>
+                    </div>
+                    </>
+                )}
             </div>
-            <p>Membre depuis le 6 mars 1452</p>
+            <p>Membre depuis le :{userData.createdAt}</p>
             <input type="button" className="followers-btn" value="Abonnements:"/>
             <input type="button" className="followers-btn" value="Abonnés :"/>
         </div>
