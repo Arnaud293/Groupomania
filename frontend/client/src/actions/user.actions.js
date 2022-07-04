4b2fb4d81fba6411c4141de9e01e0c2bd47b6857
Arnaud293 <arnaudlt293@gmail.com>
2022-07-04 13:04:41 +0200

    PUT: UpdateBio from <p> to <textarea> to <p> using REDUX

--- frontend/client/src/actions/user.actions.js
+++ frontend/client/src/actions/user.actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const GET_USER = 'GET_USER';
 export const UPLOAD_PICTURE = 'UPLOAD_PICTURE';
+export const UPDATE_BIO = 'UPDATE_BIO';
 
 export const getUser = (uid) =>{
     return (dispatch) => {
@@ -28,3 +29,20 @@ export const uploadPicture = (data, id) =>{
         .catch((err) => console.log(err));
     }
 }
+
+export const updateBio = (userId, bio) => {
+    return (dispatch) => {
+    return axios({
+        method : "put",
+        url : `${process.env.REACT_APP_API_URL}api/user/` + userId,
+        data : {bio}
+
+    })
+    .then((res) => {
+        dispatch({
+            type: UPDATE_BIO, payload : bio
+        })
+    })
+    .catch ((err) => console.log(err));
+    }
+};
