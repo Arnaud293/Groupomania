4ad8b0a9a5a4c14f1ce79d4cbf35ecc46fab003d
Arnaud293 <arnaudlt293@gmail.com>
2022-07-20 14:30:04 +0200

    CRUD-STORE: case delete and edit comment

--- frontend/client/src/reducers/post.reducer.js
+++ frontend/client/src/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { DELETE_POST, GET_POSTS, LIKE_POST, UNLIKE_POST, UPDATE_POST } from "../actions/post.actions";
+import { DELETE_COMMENT, DELETE_POST, EDIT_COMMENT, GET_POSTS, LIKE_POST, UNLIKE_POST, UPDATE_POST } from "../actions/post.actions";
 
 const initialState = {};
 
@@ -41,6 +41,34 @@ export default function postReducer(state = initialState, action){
 
         case DELETE_POST :
             return state.filter((post) => post._id !== action.payload.postId)
+
+        case EDIT_COMMENT :
+            return state.map((post) => {
+                if(post._id === action.payload.postId){
+                    return{
+                        ...post,
+                        comments : post.comments.map((comment) => {
+                            if(comment._id === action.payload.commentId){
+                                return {
+                                    ...comment,
+                                    text : action.payload.text
+                                }
+                            }
+                            else return comment;
+                        })
+                    }
+                } else return post;
+            })
+
+        case DELETE_COMMENT :
+            return state.map((post) => {
+                if(post._id === action.payload.postId){
+                    return{
+                        ...post,
+                        comments : post.comments.filter((comment) => comment._id !== action.payload.commentId)
+                    }
+                } else return post;
+            })
 
         default : 
         return state;
